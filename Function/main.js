1adfc61a4e18fad65f342b8de5c02b10d66c2824
HorizonZeroBrawn <78885017+HorizonZeroBrawn@users.noreply.github.com>
2021-09-03 10:51:18 -0600

    Updated variables + saving progress

--- Function/main.js
+++ Function/main.js
@@ -10,37 +10,37 @@ using the test function on the input string and then executing the correct funct
 //BONUS: store previous answers and allow user to call on previous results for future calculations
 
 function ultimateCalculator() {
-    let values = document.getElementById("totalInput").value;
+    let input = document.getElementById("totalInput").value;
     var plusOperator = /\+/;
     var minusOperator = /\-/;
     var multiplicationOperator = /\*/;
     var divisionOperator = /\//;
 
-    if (plusOperator.test(values)) {
-        let arr = values.split("+");
-        addition = () => parseInt(arr[0])+parseInt(arr[1]);
+    if (plusOperator.test(input)) {
+        let listOfNumbers = input.split("+");
+        addition = () => parseInt(listOfNumbers[0]) + parseInt(listOfNumbers[1]) + parseInt(listOfNumbers[2]);
         if (`${addition()}` === "NaN") {
             alert `Looks like you have a hard time following instructions, fam.`; //accounts for edge case error message where the operator is present but the input is invalid
         } else document.getElementById("p1").innerHTML = addition();
     } 
-    else if (minusOperator.test(values)) {
-        let arr = values.split("-");
-        subtraction = () => parseInt(arr[0])-parseInt(arr[1]);
+    else if (minusOperator.test(input)) {
+        let listOfNumbers = input.split("-");
+        subtraction = () => parseInt(listOfNumbers[0])-parseInt(listOfNumbers[1]);
         if (`${subtraction()}` === "NaN") {
             alert `Looks like you have a hard time following instructions, fam.`;
         } else document.getElementById("p1").innerHTML = subtraction();
     } 
-    else if (multiplicationOperator.test(values)) {
-        let arr = values.split("*");
-        multiplication = () => parseInt(arr[0])*parseInt(arr[1]);
+    else if (multiplicationOperator.test(input)) {
+        let listOfNumbers = input.split("*");
+        multiplication = () => parseInt(listOfNumbers[0])*parseInt(listOfNumbers[1]);
         if (`${multiplication()}` === "NaN") {
             alert `Looks like you have a hard time following instructions, fam.`;
         } else document.getElementById("p1").innerHTML = multiplication();
     } 
-    else if (divisionOperator.test(values)) {
-        let arr = values.split("/");
-        division = () => parseInt(arr[0])/parseInt(arr[1]);
-        remainder = () => parseInt(arr[0])%parseInt(arr[1]);
+    else if (divisionOperator.test(input)) {
+        let listOfNumbers = input.split("/");
+        division = () => parseInt(listOfNumbers[0])/parseInt(listOfNumbers[1]);
+        remainder = () => parseInt(listOfNumbers[0])%parseInt(listOfNumbers[1]);
         if (`${division()}` === "NaN") {
             alert `Looks like you have a hard time following instructions, fam.`;
         }else document.getElementById("p1").innerHTML =`The answer is ${division()} with the remainder of ${remainder()}`;
